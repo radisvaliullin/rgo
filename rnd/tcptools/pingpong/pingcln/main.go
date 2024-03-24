8dc4974ffa4407fdbc524f20104ba9cf6e2a2060
Radis Valiullin <radisrower@gmail.com>
2024-03-24 08:00:19 -0700

    rnd: tcptools: pingpong: ping cln and pong server, simpel tcp client server

---
+++ rnd/tcptools/pingpong/pingcln/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+// simple tcp client
+// connect to server and send message
+// expect to get copy message
+// message read from stdin
+func main() {
+	fmt.Println("ping client start.")
+
+	// config
+	addr := flag.String("addr", ":4000", "server address")
+	flag.Parse()
+
+	conn, err := net.Dial("tcp", *addr)
+	if err != nil {
+		log.Fatalf("ping: dial conn err: %v", err)
+	}
+
+	if err := session(conn); err != nil {
+		log.Fatalf("ping: session err: %v", err)
+	}
+}
+
+func session(conn net.Conn) error {
+	bufSize := 2048
+	readBuf := make([]byte, bufSize)
+
+	input := bufio.NewReaderSize(os.Stdin, bufSize)
+
+	for {
+		in, err := input.ReadString('\n')
+		if err != nil {
+			log.Printf("session: read input err: %v", err)
+			return err
+		}
+
+		_, err = conn.Write([]byte(in))
+		if err != nil {
+			log.Printf("session: write to conn err: %v", err)
+			return err
+		}
+
+		n, err := conn.Read(readBuf)
+		if err != nil {
+			log.Printf("session: read from conn err: %v", err)
+			return err
+		}
+		log.Printf("sessions: response - %v", string(readBuf[:n]))
+	}
+}
---
+++ rnd/tcptools/pingpong/pongsrv/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+)
+
+// simple tcp server
+// accept connect and read from conn
+// respond to request with copy message
+func main() {
+	fmt.Println("start pong server.")
+
+	// config
+	addr := flag.String("addr", ":4000", "server address")
+	flag.Parse()
+
+	ln, err := net.Listen("tcp", *addr)
+	if err != nil {
+		log.Fatalf("listen fail: %v", err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Printf("accept fail: %v", err)
+		}
+
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	log.Printf("pong: conn accepted: local - %v, remote - %v", conn.LocalAddr(), conn.RemoteAddr())
+	defer func() {
+		log.Printf("pong: conn disconnected: local - %v, remote - %v", conn.LocalAddr(), conn.RemoteAddr())
+	}()
+
+	bufSize := 2048
+	readBuf := make([]byte, bufSize)
+
+	for {
+
+		// read
+		n, err := conn.Read(readBuf)
+		if err != nil {
+			log.Printf("pong: read err: %v", err)
+			return
+		}
+		log.Printf("pong: read message - %v", string(readBuf[:n]))
+
+		// write copy message
+		_, err = conn.Write(readBuf[:n])
+		if err != nil {
+			log.Printf("pong: write err: %v", err)
+			return
+		}
+	}
+}
